763533fa4e6f208d2178c86085a27ce00aebb45d
madhusekhar kothakota <madhukothakota456@gmail.com>
2026-07-19 14:39:15 +0530

    encapsulation

---
+++ encapsulation1.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+class Employee
+{
+    private:
+        int salary;
+    public:
+        void setSalary(int s)
+        {
+            salary = s;
+        }
+        int getSalary()
+        {
+            return salary;
+        }
+};
+int main()
+{
+    Employee emp;
+    emp.setSalary(50000);
+    cout << "Employee Salary: " << emp.getSalary() << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+}
